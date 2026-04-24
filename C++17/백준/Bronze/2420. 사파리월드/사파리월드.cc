f2da5deea18a9233699b66da1f44990a6397d286
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 22:48:43 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    ll a, b;
+    cin >> a >> b;
+    cout << abs(a - b);
+    return 0;
+}
