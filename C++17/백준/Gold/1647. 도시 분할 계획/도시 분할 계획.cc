a5346da812b5ffb1ab0e61f64dbbdb1bca869b21
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 20:55:01 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 728 ms, Memory: 106436 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+vector<pair<ll, ll>> v[100005];
+ll arr[100005];
+ll a, b, x, y, z, n, M = 0;
+
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    n = a;
+    while (b--) {
+        cin >> x >> y >> z;
+        v[x].push_back({z, y});
+        v[y].push_back({z, x});
+    }
+    fill_n(arr, a+1, 1000005);
+    for (ll i = 0; i < v[1].size(); i++) {
+        pq.push(v[1][i]);
+    }
+    arr[1] = 0;
+    arr[0] = 0;
+    a--;
+    while (a != 0) {
+        ll cost = pq.top().first;
+        ll to = pq.top().second;
+        pq.pop();
+        if (arr[to] == 1000005) {
+            arr[to] = cost;
+            M = max(cost, M);
+            arr[0] += cost;
+            for (ll i = 0; i < v[to].size(); i++) {
+                pq.push(v[to][i]);
+            }
+            a--;
+        }
+    }
+    cout << arr[0] - M;
+    return 0;
+}
