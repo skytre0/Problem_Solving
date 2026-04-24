26cd442ce8ab34a0c0fc5e901488bd5735ad7d4c
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 23:11:00 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    string s = "";
+    getline(cin, s);
+    if (s == "8 7 6 5 4 3 2 1")
+        cout << "descending";
+    else if (s == "1 2 3 4 5 6 7 8")
+        cout << "ascending";
+    else
+        cout << "mixed";
+    return 0;
+}    
