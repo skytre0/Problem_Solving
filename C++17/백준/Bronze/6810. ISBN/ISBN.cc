83fb0cea6434bacd099cd88a11f24c677de2b733
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 23:59:10 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b >> c;
+    cout << "The 1-3-sum is " << 91 + a + b * 3 + c;
+    return 0;
+}
