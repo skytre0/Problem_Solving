99736036b059e2cd2ba6f9c2795d4503cf4ec299
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 23:57:08 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b;
+string s;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    cout << b + (b - a);
+    return 0;
+}
