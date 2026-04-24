1fa273fa5afabb88991c8ca052a704cd26cfa407
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 22:47:52 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll arr[100005];
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cout << "  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\";
+    return 0;
+}
