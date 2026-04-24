5dd26d0fc0047cfb039f7c6dfe8b7e97ade68497
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 20:46:54 +0900

    [Bronze II] Title: 단어의 개수, Time: 16 ms, Memory: 3680 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    string s;
+    int a = 0;
+    while (cin >> s) {a++;}
+    cout << a;
+    return 0;
+}
