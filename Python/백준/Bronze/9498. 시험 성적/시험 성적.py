118658e40f23b9233717132e32778b47cd8df65e
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 00:05:46 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+a = int(input())
+
+if 60 <= a < 70:
+    print("D")
+elif 70 <= a < 80:
+    print("C")
+elif 80 <= a < 90:
+    print("B")
+elif 90 <= a:
+    print("A")
+else:
+    print("F")
+    
