73f82e06855830390de252d47454d8055a1139c8
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 00:04:30 +0900

    [Bronze V] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int l, a, b, c, d;
+string s;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+        cout << "Hello World, Judge " << i << "!\n";
+    return 0;
+}
