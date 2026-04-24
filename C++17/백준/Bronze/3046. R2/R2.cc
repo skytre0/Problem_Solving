7e4d8aa28417e09fdc8e28baf5f1f28d070703bb
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-24 23:11:47 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    cout << b * 2 - a;
+    return 0;
+}
