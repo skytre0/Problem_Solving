b0b482aadab2678a433c8a3468f6cab581b34d52
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:34:24 +0900

    [Bronze V] Title: Correct, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26307. Correct/Correct.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    cout << (a - 9) * 60 + b;
+    return 0;
+}
