784028322f44b854f8da13ba80c62a53ae5ba2b0
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:13:54 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll a, b;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    cout << a * b / 2;
+    return 0;
+}
