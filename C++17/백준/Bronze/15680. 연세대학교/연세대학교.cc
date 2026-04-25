71a38a6bb5c333391386c21cdfb5378d1d3fb985
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 17:51:45 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, n, cnt = 0;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> n;
+    if (n)
+        cout << "Leading the Way to the Future";
+    else
+        cout << "YONSEI";
+    return 0;
+}
