014a396009b4b65d7c7852863eae57ff1f030698
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 17:47:20 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+string s;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> s;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << s << "::fan:\n";
+    cout << ":fan::fan::fan:";
+    return 0;
+}
