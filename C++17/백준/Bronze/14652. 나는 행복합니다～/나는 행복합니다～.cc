02ff597ba1a088d182a5f9f561cf730a16ec5b61
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 17:48:01 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b >> c;
+    cout << c / b << " " << c % b;
+    return 0;
+}
