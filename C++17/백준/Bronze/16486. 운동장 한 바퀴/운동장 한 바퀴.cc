e281a2f259496c3f7c23af9e3db66d8b46c41a3e
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:18:05 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    float pi = 3.141592;
+    cin >> a >> b;
+    cout << fixed << setprecision(6) << 2 * a + 2 * pi * b;
+    return 0;
+}
