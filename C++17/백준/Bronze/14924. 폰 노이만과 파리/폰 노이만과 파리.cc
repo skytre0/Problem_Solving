7675e74faedd49e33d7c29d75faffbe32fbb56d3
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 17:49:04 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b >> c;
+    cout << b * (c / 2 / a);
+    return 0;
+}
