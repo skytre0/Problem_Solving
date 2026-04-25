a936fe0dc8b54e493566e7a6e3e63c5d232c51a1
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:23:36 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a;
+    if (a / 10000 == 555)   cout << "YES";
+    else    cout << "NO"; 
+    return 0;
+}
