c7c8281394348f4ff821e4db2fe9e4622a4db821
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:17:35 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll a;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a;
+    cout << a - 1946;
+    return 0;
+}
