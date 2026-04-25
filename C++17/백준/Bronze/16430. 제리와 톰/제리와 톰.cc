caf6e9ee5a5ebbc0a54ac86f2e10972c20db4202
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:17:50 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, n, cnt = 0;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    cout << b-a << " " << b;
+    return 0;
+}
