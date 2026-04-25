0cc0209d06aefee5eba2870aee00c03276e79074
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 17:48:43 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2424 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,45 @@
+#include <string>
+#include <set>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int arr[9] = {0};
+int arr1[9] = {0};
+set<string> s;
+int a, b, c;
+
+int solve(int x){
+    string ss = "";
+    if (x == b) {
+        for (size_t i = 0; i < b; i++)
+            ss = ss + to_string(arr[i]) + " ";
+        c = s.size();
+        s.insert(ss);
+        if (s.size() != c)
+            cout << ss << "\n";
+    }
+    else {
+        for (size_t i = 0; i < a; i++) {
+            if (x != 0)
+                arr[x] = arr[x-1];
+            if (arr[x] <= arr1[i]) {
+                arr[x] = arr1[i];
+                solve(x + 1);
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a >> b;
+    for (size_t i = 0; i < a; i++)
+        cin >> arr1[i];
+    sort(arr1, arr1+a);
+    solve(0);
+    return 0;
+}
