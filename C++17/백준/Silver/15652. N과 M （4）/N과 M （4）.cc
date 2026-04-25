31de3c5e21fba6c9d1a968c9a89386a7086c1eec
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 17:51:07 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    int arr[9] = {0};
+    int a, b, count, out = 0;
+    cin >> a >> b;
+    for (size_t i = 1; i <= a; i++)
+        arr[i] = 1;
+    while (1) {
+        count = 0;
+        for (size_t i = 1; i <= b; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+        arr[b]++;
+        while (arr[b-count] > a && b-count > 0) {
+            count++;
+            arr[b-count]++;
+        }
+        if (b-count == 0)
+            break;
+        if (count > 0) {
+            for (size_t i = b-count+1; i <= b; i++) 
+                arr[i] = arr[i-1];
+        }
+    }
+    return 0;
+}
