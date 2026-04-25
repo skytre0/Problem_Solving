af0d09ee76697bf1ad5f5979a93b0e2445e7c8be
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:34:54 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        cin >> b;
+        cout << b << " " << b << "\n";
+    }
+    return 0;
+}
