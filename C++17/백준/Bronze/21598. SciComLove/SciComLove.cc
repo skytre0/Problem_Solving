0080f7be790904643cf2278f81e36fd848d63ce0
Seong-Ji Yang <157494322+skytre0@users.noreply.github.com>
2026-04-25 20:27:13 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ C++17/백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll a, b, n;
+string s = "SciComLove\n";
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> n;
+    for (ll i = 0; i < n; i++) {
+        cout << s;
+    }
+    return 0;
+}
